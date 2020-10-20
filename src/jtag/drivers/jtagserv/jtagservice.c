aa48879a303a67e93642b0228faab61d8e8430a3
Ooi, Cinly <cinly.ooi@intel.com>
2020-10-20 17:20:40 +0800

    jtagservice_query_main(): Working for AJI_CLAIM
    Not working for AJI_CLAIM2.
    
    Can retrieve IDCODE correctly. Previously using bad IDCODE IR
    command.
    
    Can recover from TAP access error. Previously if this TAP
    has an error, other subsequent TAP will also fail. Reason is
    chain_id will be unlocked when an error occur, and on the next
    TAP the code did not lock it again
    
    Signed-off-by: Ooi, Cinly <cinly.ooi@intel.com>

--- src/jtag/drivers/jtagserv/jtagservice.c
+++ src/jtag/drivers/jtagserv/jtagservice.c
@@ -213,27 +213,6 @@ AJI_ERROR  jtagservice_free(jtagservice_record *me, DWORD timeout)
     
     jtagservice_unlock(me, ALL, timeout);
 
-    //TODO: Free the variables    
-    if (me->hardware_count != 0) {
-        free(me->hardware_list);
-        free(me->server_version_info_list);
-
-        me->hardware_count = 0;
-        me->hardware_list = NULL;
-        me->server_version_info_list = NULL;
-    }
-
-    if(me->device_count != 0) {
-        free(me->device_list);
-        free(me->device_open_id_list);
-        free(me->device_type_list);
-
-        me->device_count = 0;
-        me->device_list = NULL;
-        me->device_open_id_list = NULL;
-        me->device_type_list = NULL;
-    }
-
     if (me->claims_count) {
         for (DWORD i = 0; i < me->claims_count; ++i) {
             if (0 != me->claims[i].claims_n) {
@@ -244,9 +223,27 @@ AJI_ERROR  jtagservice_free(jtagservice_record *me, DWORD timeout)
         }
     }
 
-#if IS_WIN32
-    c_jtag_client_gnuaji_free();
-#endif
+    //TODO: Free the variables    
+
+    if (me->device_count != 0) {
+        free(me->device_list);
+        free(me->device_open_id_list);
+        free(me->device_type_list);
+
+        me->device_count = 0;
+        me->device_list = NULL;
+        me->device_open_id_list = NULL;
+        me->device_type_list = NULL;
+    }
+
+    if (me->hardware_count != 0) {
+        free(me->hardware_list);
+        free(me->server_version_info_list);
+
+        me->hardware_count = 0;
+        me->hardware_list = NULL;
+        me->server_version_info_list = NULL;
+    }
 
     return AJI_NO_ERROR;
 }
@@ -291,51 +288,52 @@ int jtagservice_query_main(void) {
                    i+1, hw.device_name, hw.hw_name, hw.server, hw.port,  hw.chain_id, (unsigned long) hw.persistent_id, hw.chain_type, (unsigned long) hw.features,
                    server_version_info_list[i]);
            
-           //Assume if persistent_id==0,hw is not defined. Felt that .port="Unable to connect" 
-           //..is not a strong enough indicator that something is wrong       
-           if(hw.persistent_id == 0) {
+            //Assume if persistent_id==0,hw is not defined. Felt that .port="Unable to connect" 
+            //..is not a strong enough indicator that something is wrong       
+            if(hw.persistent_id == 0) {
                printf("        Not a valid device.\n");
                continue;
-           }
-           AJI_CHAIN_ID chain_id = hw.chain_id;
-           status = c_aji_lock_chain(chain_id, timeout);
-           if(AJI_NO_ERROR !=  status ) { 
-               printf("       Problem with chain locking. Returned %d\n", status);
-               continue;
-           }
+            }
+            AJI_CHAIN_ID chain_id = hw.chain_id;
+            status = c_aji_lock_chain(chain_id, timeout);
+            if(AJI_NO_ERROR !=  status ) { 
+                printf("       Problem with chain locking. Returned %d (%s)\n", status, c_aji_error_decode(status));
+                continue;
+            }
 
-           DWORD device_count = hardware_capacity;
-           AJI_DEVICE *device_list = (AJI_DEVICE*) calloc(device_count, sizeof(AJI_DEVICE));
-           status = c_aji_read_device_chain(chain_id, &device_count, device_list, 1);
-           if(AJI_NO_ERROR !=  status ) { 
-               printf("       Problem with Getting device on this chain. Returned %d\n", status);
-               free(device_list);
-               c_aji_unlock_chain(chain_id);
-               continue;
-           }
-           printf("        Number of devices on chain is %lu\n", (unsigned long) device_count);
-           for(DWORD tap_position=0; tap_position<device_count; ++tap_position) {
-               AJI_DEVICE device = device_list[tap_position];
-               printf("        (A%lu) device_id=%08lX, instruction_length=%d, features=%lu, device_name=%s\n", 
+            DWORD device_count = hardware_capacity;
+            AJI_DEVICE *device_list = (AJI_DEVICE*) calloc(device_count, sizeof(AJI_DEVICE));
+            status = c_aji_read_device_chain(chain_id, &device_count, device_list, 1);
+            if(AJI_NO_ERROR !=  status ) { 
+                printf("       Problem with eetting device on this chain. Returned %d (%s)\n", status, c_aji_error_decode(status));
+                free(device_list);
+                c_aji_unlock_chain(chain_id);
+                continue;
+            }
+
+            printf("        Number of devices on chain is %lu\n", (unsigned long) device_count);
+            for(DWORD tap_position=0; tap_position<device_count; ++tap_position) {
+                AJI_DEVICE device = device_list[tap_position];
+                printf("        (A%lu) device_id=%08lX, instruction_length=%d, features=%lu, device_name=%s\n", 
                         (unsigned long) tap_position+1, (unsigned long) device.device_id, device.instruction_length,
                         (unsigned long) device.features, device.device_name
-               );
+                );
                         
                
-               DWORD hier_id_n = hardware_capacity; 
-               AJI_HIER_ID *hier_ids = (AJI_HIER_ID*) calloc(hier_id_n, sizeof(AJI_HIER_ID));
-               AJI_HUB_INFO *hub_infos = (AJI_HUB_INFO*) calloc(AJI_MAX_HIERARCHICAL_HUB_DEPTH, sizeof(AJI_HUB_INFO));
+                DWORD hier_id_n = hardware_capacity; 
+                AJI_HIER_ID *hier_ids = (AJI_HIER_ID*) calloc(hier_id_n, sizeof(AJI_HIER_ID));
+                AJI_HUB_INFO *hub_infos = (AJI_HUB_INFO*) calloc(AJI_MAX_HIERARCHICAL_HUB_DEPTH, sizeof(AJI_HUB_INFO));
 
-               status =  c_aji_get_nodes_b(chain_id, tap_position, hier_ids, &hier_id_n, hub_infos);
-               if(AJI_NO_ERROR !=  status ) { 
-                   printf("       Problem with Getting nodes for this tap position. Returned %d\n", status);
-                   free(hier_ids);
-                   free(hub_infos);
-                   continue;
-               }
+                status =  c_aji_get_nodes_b(chain_id, tap_position, hier_ids, &hier_id_n, hub_infos);
+                if(AJI_NO_ERROR !=  status ) { 
+                    printf("       Problem with getting nodes for this tap position. Returned %d (%s)\n", status, c_aji_error_decode(status));
+                    free(hier_ids);
+                    free(hub_infos);
+                    continue;
+                }
                
-               printf("            Number of SLD nodes (hier_id_n)=%lu,\n", (unsigned long) hier_id_n); //hier_id_n = 0 and status=AJI_NO_ERROR if no SLD_HUB
-               for(DWORD k=0; k<hier_id_n; ++k) { //With ARRIA10, this loop is entered for the FPGA Tap, if it has SLD nodes.
+                printf("            Number of SLD nodes (hier_id_n)=%lu,\n", (unsigned long) hier_id_n); //hier_id_n = 0 and status=AJI_NO_ERROR if no SLD_HUB
+                for(DWORD k=0; k<hier_id_n; ++k) { //With ARRIA10, this loop is entered for the FPGA Tap, if it has SLD nodes.
                     AJI_HIER_ID hid = hier_ids[k];
                     printf("            (B%lu) idcode=%08lX position_n=%lu position: ",
                              (unsigned long) k+1,
@@ -350,100 +348,105 @@ int jtagservice_query_main(void) {
                         printf(" (Hub %d) bridge_idcode=%08lX, hub_id_code=%08lX", m, m == 0 ? 0 : (hub_infos[k].bridge_idcode[m]), (hub_infos[k].hub_idcode[m]));
                     } //end for m (bridge)
                     printf("\n");
-               } //end for k (heir_id_n)
-               
-                if(hier_id_n == 0) { 
-                    //With ARRIA10, this loop is entered for the ARM HPS TAP.
-                    //HOWEVER, it will also be entered if the FPGA TAP has no SLD Hub.
-                    int claim_size = 2;
-                    AJI_CLAIM2 claims[] = { //setup for HPS
-                          { AJI_CLAIM_IR_SHARED,          0, device.device_id == 0x4BA00477? 0b1111 : 0b1111111111 }, //NO NEED for BYPASS instruction actually
-                          { AJI_CLAIM_IR_SHARED,          0, device.device_id == 0x4BA00477? 0b1110 : 0b1111111100 },
-                    };
+                } //end for k (heir_id_n)
 
-                    char appname[] = "MyApp";
-                    printf("            (C1-1) Open Device %lX ...\n", (unsigned long) device.device_id); fflush(stdout);
-                    AJI_OPEN_ID open_id = NULL;   
-                    
-                    status = c_aji_open_device_a(chain_id, tap_position, &open_id, claims, claim_size, appname);
-                    if(AJI_NO_ERROR  != status) {
-                       printf("            Cannot open device. Returned %d\n", status);
-                       continue;
-                    }
-                    
-                    printf("            (C1-2) Lock Device ...\n"); fflush(stdout);
-                    status = c_aji_unlock_chain_lock(chain_id, open_id, AJI_PACK_AUTO); //hardcoded timeout of 10000ms 
-                    if(AJI_NO_ERROR  != status) {
-                       printf("       Cannot lock device. Returned %d\n", status);
-                       c_aji_close_device(open_id);
-                       continue;
-                    }
-                    
-                    /* Need not set state of TAP.
-                    printf("            (C2-1) Set TAP state  TEST_LOGIC_RESET ...\n"); fflush(stdout);             
-                    //status = c_aji_run_test_idle(open_id, 10); //Doing two clock pulse. One now, and one potentially deferred until the next instruction
-                    //status = c_aji_test_logic_reset(open_id);  
-                    if(AJI_NO_ERROR  != status) {
-                       printf("       Cannot set TAP state to RUN_TEST_IDLE. Returned %d\n", status);
-                       c_aji_unlock(open_id);
-                       c_aji_close_device(open_id);
-                       continue;
-                    }
-                    */
-                    
-                    printf("            (C2-2) Read IDCODE : Set IR ...\n"); fflush(stdout);  
-                    { //begin dummy block
-                    DWORD captured_ir = 0xFFFF;      
-                    status = c_aji_access_ir(open_id, claims[1].value,  &captured_ir, 0);  
-                                //if I am in TEST_LOGIC_RESET, I am suppose to get AJI_BAD_TAP STATE but I am not, and I get 0b1 on captured_ir
-                    if(AJI_NO_ERROR  != status) {
-                       printf("       Cannot send IDCODE instruction. Returned %d\n", status);
-                       c_aji_unlock(open_id);
-                       c_aji_close_device(open_id);
-                       continue;
-                    }
-                    printf("                 Return IR buffer reads %04lX\n", (unsigned long) captured_ir);
-                    } //end dummy block
-                    
-                    printf("            (C2-3) Read IDCODE : Read DR ...\n"); fflush(stdout);  
-                    
-                    DWORD length_dr = 32,
-                          flags = 0,
-                          write_offset = 0,
-                          write_length = 0, //32,
-                          read_offset = 0,
-                          read_length = 32;
+                /* Try to relock the chain. This is to counter a problem in the for(tap_position) where 
+                  if there is an error, the chain will be unlocked and when looping through the
+                  next tap_position AJI will complain that the chain is not locked. If it is locked already,
+                  as it should be during normal operation, just ignore the AJI_LOCKED message, since the effect
+                  is the chain is locked, which is what we want
+                */
+                status = c_aji_lock_chain(chain_id, timeout);
+                if (AJI_NO_ERROR != status && AJI_LOCKED != status) {
+                    printf("       Problem attempting chain relocking. Returned %d (%s)\n", status, c_aji_error_decode(status));
+                    continue;
+                }
 
-                    BYTE  write_bits[] = { 0x55, 0x55, 0x55, 0x55, 0x55, 0x55, 0x55, 0x55 };
-                    BYTE  read_bits[]  = { 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF };
+                int claim_size = 2;
+                AJI_CLAIM claims[] = { //setup for HPS
+                      { AJI_CLAIM_IR_SHARED, device.device_id == 0x4BA00477 ? 0b1111 : 0b1111111111 }, //NO NEED for BYPASS instruction actually
+                      { AJI_CLAIM_IR_SHARED, device.device_id == 0x4BA00477 ? 0b1110 : 0b0000000110 },
+                };
+
+                char appname[] = "MyApp";
+                printf("            (C1-1) Open Device %lX ...\n", (unsigned long) device.device_id); fflush(stdout);
+                AJI_OPEN_ID open_id = NULL;   
                     
-                    status = c_aji_access_dr( open_id, length_dr, flags, write_offset, write_length, write_bits,  read_offset, read_length, read_bits  );
-                    if(AJI_NO_ERROR  != status) {
-                       printf("       Cannot receive IDCODE output. Returned %d\n", status);
-                       c_aji_unlock(open_id);
-                       c_aji_close_device(open_id);
-                       continue;
-                    }
-                    printf("                 Return DR buffer reads %02X%02X%02X%02X %02X%02X%02X%02X\n", 
+                status = c_aji_open_device(chain_id, tap_position, &open_id, claims, claim_size, appname);
+                if(AJI_NO_ERROR  != status) {
+                    printf("            Cannot open device. Returned %d (%s). This is not an error if the device is not SOCVHPS or FPGA Tap\n", status, c_aji_error_decode(status));
+                    continue;
+                }
+                    
+                printf("            (C1-2) Lock Device ...\n"); fflush(stdout);
+                status = c_aji_unlock_chain_lock(chain_id, open_id, AJI_PACK_AUTO); //hardcoded timeout of 10000ms 
+                if(AJI_NO_ERROR  != status) {
+                    printf("       Cannot lock device. Returned %d (%s)\n", status, c_aji_error_decode(status));
+                    c_aji_close_device(open_id);
+                    continue;
+                }
+                /*
+                //Need not set state of TAP.
+                printf("            (C2-1) Set TAP state  TEST_LOGIC_RESET ...\n"); fflush(stdout);             
+                status = c_aji_run_test_idle(open_id, 10); //Doing two clock pulse. One now, and one potentially deferred until the next instruction
+                status = c_aji_test_logic_reset(open_id);  
+                if(AJI_NO_ERROR  != status) {
+                   printf("       Cannot set TAP state to RUN_TEST_IDLE. Returned %d\n", status);
+                   c_aji_unlock(open_id);
+                   c_aji_close_device(open_id);
+                   continue;
+                }
+                */
+                QWORD ir_idcode = device.device_id == 0x4BA00477 ? 0b1110 : 0b0000000110; // claims[0].value;
+                printf("            (C2-2) Read IDCODE : Set IR (0x%llX)...\n", ir_idcode); fflush(stdout);
+                DWORD captured_ir = 0xFFFF;      
+                status = c_aji_access_ir(open_id, ir_idcode,  &captured_ir, 0);
+                            //if I am in TEST_LOGIC_RESET, I am suppose to get AJI_BAD_TAP STATE but I am not, and I get 0b1 on captured_ir
+                if(AJI_NO_ERROR  != status) {
+                     printf("       Cannot send IDCODE instruction. Returned %d (%s) \n", status, c_aji_error_decode(status));
+                     c_aji_unlock(open_id);
+                     c_aji_close_device(open_id);
+                     continue;
+                }
+                printf("                 Return IR buffer reads %04lX\n", (unsigned long) captured_ir);
+                
+                printf("            (C2-3) Read IDCODE : Read DR ...\n"); fflush(stdout);  
+                DWORD length_dr = 32,
+                      flags = 0,
+                      write_offset = 0,
+                      write_length = 0, //32,
+                      read_offset = 0,
+                      read_length = 32;
+
+                BYTE  write_bits[] = { 0x55, 0x55, 0x55, 0x55, 0x55, 0x55, 0x55, 0x55 };
+                BYTE  read_bits[]  = { 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF };
+
+                status = c_aji_access_dr( open_id, length_dr, flags, write_offset, write_length, write_bits,  read_offset, read_length, read_bits  );
+                if(AJI_NO_ERROR  != status) {
+                   printf("       Cannot receive IDCODE output. Returned %d (%s)\n", status, c_aji_error_decode(status));
+                   c_aji_unlock(open_id);
+                   c_aji_close_device(open_id);
+                   continue;
+                }
+                printf("                 Return DR buffer reads %02X%02X%02X%02X %02X%02X%02X%02X\n", 
                             read_bits[3], read_bits[2], read_bits[1], read_bits[0], 
                             read_bits[7], read_bits[6], read_bits[5], read_bits[4]
-                    );
+                );
            
 
-                    printf("            (C1-3) Flush Device ...\n"); fflush(stdout);
-                    c_aji_flush(open_id);
+                printf("            (C1-3) Flush Device ...\n"); fflush(stdout);
+                c_aji_flush(open_id);
+                   
+                printf("            (C1-4) Unlock Device ...\n"); fflush(stdout);
+                c_aji_unlock(open_id);
                     
-                    printf("            (C1-4) Unlock Device ...\n"); fflush(stdout);
-                    c_aji_unlock(open_id);
-                    
-                    printf("            (C1-5) Close Device ...\n"); fflush(stdout);
-                    c_aji_close_device(open_id);
-                    printf("(C1-5) Done.\n");
-               } //end if (hier_id_n == 0)
+                printf("            (C1-5) Close Device ...\n"); fflush(stdout);
+                c_aji_close_device(open_id);
+                printf("(C1-5) Done.\n");
 
-               free(hier_ids);
-               free(hub_infos);
-           } // end for tap_position (device_count)
+                free(hier_ids);
+                free(hub_infos);
+            } // end for tap_position (device_count)
                  
  
            free(device_list);
@@ -453,7 +456,7 @@ int jtagservice_query_main(void) {
 
 
     printf("Free\n");
-//    free(hardware_list);
+    free(hardware_list);
  
     printf("Completed\n");
 	return 0;
